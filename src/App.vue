f4980547dcbc14b54b273d9d67576826a8ee6073
pindang-cloud <gavinku890@gmail.com>
2024-12-09 16:03:56 +0700

    testt

--- src/App.vue
+++ src/App.vue
@@ -148,7 +148,7 @@ export default {
 
             return {
               Meja: layer.name.value || `Layer ${layerIndex + 1}`,
-              konten: validKonten, // Hanya memasukkan konten valid
+              konten: validKonten,
             };
           })
         );
